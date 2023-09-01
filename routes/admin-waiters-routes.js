3de7f8ed43e37c10b0a751de15301451dead301c
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-09-01 20:22:04 +0200

    changed my data structure to be an array that contains a key for a day and waiters array

--- routes/admin-waiters-routes.js
+++ routes/admin-waiters-routes.js
@@ -1,20 +1,19 @@
 const adminWaitersRoutes = waitersAppLogic => {
-
     const waitersRoute = async (req, res) => {
         const { username } = req.params;
         const lowerLetterName = username.toLowerCase();
-        
+
         const validateWaiterName = name => {
             const regexPattern = /^[a-zA-Z]+$/;
             const regexPatternTest = regexPattern.test(name);
-            return regexPatternTest ? regexPatternTest : false // req.flash("error", "Invalid waiter name. Please enter abcdeABCDE.");
+            return regexPatternTest ? regexPatternTest : false; // req.flash("error", "Invalid waiter name. Please enter abcdeABCDE.");
         };
-    
+
         if (username !== ":username" && validateWaiterName(username)) {
             await waitersAppLogic.insertWaiter(lowerLetterName);
             await waitersAppLogic.setWaiterName(lowerLetterName);
-        };
-    
+        }
+
         res.render("waiters", {
             waiterName: lowerLetterName,
         });
@@ -22,48 +21,67 @@ const adminWaitersRoutes = waitersAppLogic => {
 
     const selectWorkDayRoute = async (req, res) => {
         const { weekDay } = req.body;
-        const {username} = req.params;
+        const { username } = req.params;
         if (weekDay) await waitersAppLogic.selectWorkDay(weekDay);
         res.redirect(`/waiters/${username}`);
     };
 
     const daysRoute = async (req, res) => {
-        const namesOfWaiters = [[], [], [], [], [], [], []];
         const selectedDaysByWaiters = await waitersAppLogic.waitersNameLst();
-    
-        const dayIndexMapping = {
-            "monday": 0,
-            "tuesday": 1,
-            "wednesday": 2,
-            "thursday": 3,
-            "friday": 4,
-            "saturday": 5,
-            "sunday": 6
-        };
-    
+        const availableWaiters = [
+            {
+                day: "monday",
+                waiters: [],
+            },
+            {
+                day: "tuesday",
+                waiters: [],
+            },
+            {
+                day: "wednesday",
+                waiters: [],
+            },
+            {
+                day: "thursday",
+                waiters: [],
+            },
+            {
+                day: "friday",
+                waiters: [],
+            },
+            {
+                day: "saturday",
+                waiters: [],
+            },
+            {
+                day: "sunday",
+                waiters: [],
+            },
+        ];
+
         selectedDaysByWaiters.forEach(waiterDetails => {
             const selectedDay = waiterDetails.selected_day;
-            const storeDayIndex = dayIndexMapping[selectedDay];
-            namesOfWaiters[storeDayIndex].push(waiterDetails.waiters_name);
+            availableWaiters.forEach(waiter => {
+                waiter.day === selectedDay ? waiter.waiters.push(waiterDetails.waiters_name) : null;
+            });
         });
-    
+
         res.render("admin", {
-            waiterNames: namesOfWaiters,
+            waiterNames: availableWaiters,
         });
     };
 
     const resetRoute = async (req, res) => {
         await waitersAppLogic.deleteWaiters();
         res.redirect("/days");
-    }
+    };
 
     return {
         waitersRoute,
         selectWorkDayRoute,
         daysRoute,
-        resetRoute
+        resetRoute,
     };
 };
-
 
 export default adminWaitersRoutes;
