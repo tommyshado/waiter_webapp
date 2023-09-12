10379aef4231ee00b151346d4690dd3955b79d04
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-09-12 13:05:51 +0200

    added the login route functionality

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,44 @@
 
-const loginRoute = () => {
+const loginRoute = waitersAppLogic => {
     const homeRoute = (req, res) => {
-        res.render("index");
+        res.render("index", {
+            errorMessage: req.flash("error")[0]
+        });
     };
+
+    const sendLogin = async (req, res) => {
+        const { emailOrName, password } = req.body;
+
+        const validateWaiterName = () => {
+            const regexPattern = /^[a-zA-Z]+$/;
+            const regexPatternTest = regexPattern.test(emailOrName);
+            return regexPatternTest;
+        };
+
+        if (emailOrName && validateWaiterName()) {
+            const details = {
+                emailOrName,
+                password
+            };
+
+            const adminOrWaiter = await waitersAppLogic.insertWaiter(details);
+            await waitersAppLogic.setWaiterId(details.emailOrName);
+
+            // when the waiter is found redirect to the waiters page
+            if (adminOrWaiter === "waiter") res.redirect(`/waiters/${emailOrName}`);
+            // otherwise, redirect to the admins page
+            else res.redirect("/days");
+
+        } else {
+            req.flash("error", "Invalid waiter name. Please enter name. abcdeABCDE.");
+            res.redirect("/");
+        };
+        
+    };
+
     return {
-        homeRoute
+        homeRoute,
+        sendLogin
     };
 };
 
