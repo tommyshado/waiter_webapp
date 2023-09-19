1fa83f035d9e1d5343d2e4349791153fd51d714e
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-09-19 14:21:47 +0200

    changed retrieveWaiter() to retrieveHash() and retrieved the hashed password

--- services/waiterRegistration.js
+++ services/waiterRegistration.js
@@ -3,17 +3,15 @@ const WaiterRegistration = db => {
     // insert a waiter name and hashed password into the waiters table if not in the waiters table
 
     const registerWaiter = async register => {
-        await db.none(`insert into workers (waiter_name, password, role) values ('${register.name}', '${register.hashedPassword}', 'waiter') on conflict do nothing`);
+        await db.none(`insert into waiter_registration (waiter_name, password, role) values ('${register.name}', '${register.hash}', 'waiter') on conflict do nothing`);
     };
 
     // retrieve a waiter name and hashed password from the waiters table
-    const retrieveWaiter = async () => {
-
-    };
+    const retrieveHash = async (name) => await db.oneOrNone(`select password from waiter_registration where waiter_name = $1`, name);
 
     return {
         registerWaiter,
-        retrieveWaiter,
+        retrieveHash,
     }
 
 };
