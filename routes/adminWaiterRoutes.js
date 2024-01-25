8d946344b2462a47ba9c21b7697bb3ba3d8bb28c
Mthunzi Tom <mthunzitom.10@gmail.com>
2024-01-25 17:13:40 +0200

    Added toggleMenu function in main.handlebars

--- routes/adminWaiterRoutes.js
+++ routes/adminWaiterRoutes.js
@@ -129,7 +129,7 @@ const adminWaitersRoutes = (waitersAppLogic, WaiterRosterRegistration, regexPatt
         });
 
         res.render("admin", {
-            waiterNames: availableWaiters,
+            waitersData: availableWaiters,
         });
     };
 
